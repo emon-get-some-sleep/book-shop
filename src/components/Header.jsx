f2b13d3b3447c2188bb0fe5ac7aa56275066fea0
emonh0que <you@example.com>
2023-04-04 19:29:47 +0600

    added NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import {BoltIcon} from "@heroicons/react/24/solid"
+import React from "react";
+import { BoltIcon } from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
 const Header = () => {
-    return (
-        <div className='flex items-center justify-between'>
-            {/* Logo section  */}
-            <div className='inline-flex items-center justify-between'>
-                <BoltIcon className='h-6 w-6 text-blue-500' />
-                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>next page</span>
-            </div>
+  return (
+    <div className="flex items-center justify-between">
+      {/* Logo section  */}
+      <div className="inline-flex items-center">
+        <Link to="/" className="inline-flex items-center ">
+          <BoltIcon className="h-6 w-6 text-blue-500" />
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+            nextPage
+          </span>
+        </Link>
+      </div>
 
-            {/* Nav item section  */}
-
-
-        </div>
-    );
+      {/* Nav item section  */}
+       <ul className="items-center hidden space-x-8 lg:flex">
+        <li>
+          <NavLink
+          to="/"
+          className={({isActive}) => (isActive ? 'active' : 'defualt')}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+          to="/books"
+          className={({isActive}) => (isActive ? 'active' : 'defualt')}
+          >
+            Books
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+          to="/about"
+          className={({isActive}) => (isActive ? 'active' : 'defualt')}
+          >
+            About us
+          </NavLink>
+        </li>
+       </ul>
+    </div>
+  );
 };
 
 export default Header;
