fbf46d66266a828042f904c8d0f7ce8ef205a5ba
emonh0que <you@example.com>
2023-04-04 15:20:14 +0600

    added basic router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
