eb6997642a0d893dcceb9fa377dc015e968c17e3
emonh0que <you@example.com>
2023-04-04 16:07:33 +0600

    nav logo section done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import {BoltIcon} from "@heroicons/react/24/solid"
 const Header = () => {
     return (
         <div className='flex items-center justify-between'>
-            {/* hLogo section  */}
-            <div>
-                <BoltIcon  />
-                <span>next page</span>
+            {/* Logo section  */}
+            <div className='inline-flex items-center justify-between'>
+                <BoltIcon className='h-6 w-6 text-blue-500' />
+                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>next page</span>
             </div>
 
             {/* Nav item section  */}
+
+
         </div>
     );
 };
